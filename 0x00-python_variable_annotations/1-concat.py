236898ee1717cace69550cef33e8626972692127
peluza <edisonisaza@gmail.com>
2021-03-15 21:49:10 -0500

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""1-concat.py"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat
+
+    Args:
+        str1 (str): string
+        str2 (str): string
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
