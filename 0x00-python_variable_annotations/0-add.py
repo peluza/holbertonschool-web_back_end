405f2c04b90794c45f8819d951aafddcc9a14092
peluza <edisonisaza@gmail.com>
2021-03-15 21:37:14 -0500

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""add duntion"""
+
+def add(a: float, b:float) -> float:
+    """add
+
+    Args:
+        a (float): first numbre
+        b (float): second number
+
+    Returns:
+        float:  their sum as a float.
+    """
+    return a + b
