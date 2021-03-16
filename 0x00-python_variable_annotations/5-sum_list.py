3f82fedabb9e8eb18eb53fc2772aa035e5ba8d4f
peluza <edisonisaza@gmail.com>
2021-03-15 22:17:47 -0500

    Complex types - list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""5-sum_list.py"""
+
+import typing
+
+
+def sum_list(input_list: typing.List[typing.Union[float]]) -> float:
+    """sum_mixed_list
+
+    Args:
+        mxd_lst (typing.List[typing.Union[float]]): floats as argument
+
+    Returns:
+        float: their sum as a float.
+    """
+    return sum(input_list)
