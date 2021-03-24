913e2983f7ab4356beedfb03ab00184cda605375
peluza <edisonisaza@gmail.com>
2021-03-24 09:09:18 -0500

    Simple helper function

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""simple helper function"""
+
+
+from typing import Tuple
+
+def index_range(page : int, page_size: int) -> Tuple[int, int]:
+    """index_range
+
+    Args:
+        page (int): number page
+        page_size (int): number siza pages
+
+    Returns:
+        Tuple[int, int]: print res for the pages
+    """
+    return ((page_size * (page - 1)), page_size * page)
