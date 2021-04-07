970572a86aaa60356d41401c1367f411513b4639
peluza <edisonisaza@gmail.com>
2021-04-06 20:56:46 -0500

    resore comment in the code

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -9,10 +9,16 @@ class Auth():
     """Auth"""
 
     def require_auth(
-            self,
-            path: str,
-            excluded_paths: typing.List[str]) -> bool:
-        """require_auth"""
+            self, path: str, excluded_paths: typing.List[str]) -> bool:
+        """require_auth
+
+        Args:
+            path (str):
+            excluded_paths (typing.List[str]):
+
+        Returns:
+            bool:
+        """
         if not path:
             return True
         if not excluded_paths or len(excluded_paths) == 0:
@@ -30,7 +36,14 @@ class Auth():
                     return True
 
     def authorization_header(self, request=None) -> str:
-        """authorization_header"""
+        """authorization_header
+
+        Args:
+            request ([type], optional): Defaults to None.
+
+        Returns:
+            str:
+        """
         if request is None:
             return None
         auth_header = request.headers.get('Authorization')
@@ -40,5 +53,9 @@ class Auth():
             return auth_header
 
     def current_user(self, request=None) -> typing.TypeVar('User'):
-        """current_user"""
+        """current_user
+
+        Returns:
+            None: None
+        """
         return None
