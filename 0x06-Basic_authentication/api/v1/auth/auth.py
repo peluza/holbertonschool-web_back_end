38ecbce2230ec2881383dc97678c575494162881
peluza <edisonisaza@gmail.com>
2021-04-06 20:39:28 -0500

    add auth and add information in app and index for routes

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""auth"""
+
+from flask import request
+import typing
+
+
+class Auth():
+    """Auth"""
+
+    def require_auth(
+            self,
+            path: str,
+            excluded_paths: typing.List[str]) -> bool:
+        """require_auth
+
+        Args:
+            path (str):
+            excluded_paths (typing.List[str]):
+
+        Returns:
+            bool:
+        """
+        if not path:
+            return True
+        if not excluded_paths or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            path = path + "/"
+        if path in excluded_paths:
+            return False
+        else:
+            for p in excluded_paths:
+                val = p.find("*")
+                if p.endswith("*") and p[:val] == path[:val]:
+                    return False
+                else:
+                    return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header
+
+        Args:
+            request ([type], optional): Defaults to None.
+
+        Returns:
+            str:
+        """
+        if request is None:
+            return None
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
+            return None
+        else:
+            return auth_header
+
+    def current_user(self, request=None) -> typing.TypeVar('User'):
+        """current_user
+
+        Returns:
+            None: None
+        """
+        return None
