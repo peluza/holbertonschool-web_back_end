861efe69d83100d2e1177f8b832506de2a6e7ce7
peluza <edisonisaza@gmail.com>
2021-04-18 22:38:14 -0500

    Register user

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""app"""
+
+from flask import Flask, jsonify, request
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+@app.route('/', methods=['GET'])
+def message() -> str:
+    """message
+
+    Returns:
+        str: message
+    """
+    return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email,password)
+        return jsonify({"email": "<registered email>", "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}),400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
