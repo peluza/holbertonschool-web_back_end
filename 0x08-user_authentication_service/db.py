1a6f5b4c13825d28c796758c177e4cd29072a10d
peluza <edisonisaza@gmail.com>
2021-04-18 21:20:07 -0500

    Find user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import User
 from user import Base
@@ -32,8 +33,32 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Method add_user that returns a User object"""
+        """add_user
+
+        Args:
+            email (str):
+            hashed_password (str):
+
+        Returns:
+            User:
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """find_user_by
+
+        Raises:
+            NoResultFound:
+
+        Returns:
+            User:
+        """
+        user_query = self._session.query(User).filter_by(**kwargs)
+        user = user_query.first()
+        if user is None:
+            raise NoResultFound
+        else:
+            return user
