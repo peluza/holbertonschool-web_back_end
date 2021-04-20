0c6d5cfcbf05c2bdf9f66d1a5a5a997774c44320
peluza <edisonisaza@gmail.com>
2021-04-19 19:20:09 -0500

    Find user by session ID

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -97,3 +97,21 @@ class Auth:
             return None
         self._db.update_user(user.id, session_id=_generate_uuid())
         return user.session_id
+
+    def get_user_from_session_id(self, session_id:str) -> str:
+        """get_user_from_session_id
+
+        Args:
+            session_id (str):
+
+        Returns:
+            str: user
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
+
