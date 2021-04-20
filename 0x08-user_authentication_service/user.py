3c83e9263d9b701b522f55e863093a2fe95a6949
peluza <edisonisaza@gmail.com>
2021-04-19 20:10:49 -0500

    change menors

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 """user"""
 
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
 Base = declarative_base()
 
 
 class User(Base):
-    """User"""
-    __tablename__ = "users"
+    """User
+
+    Args:
+        Base (obj): base declarative base
+    """
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
