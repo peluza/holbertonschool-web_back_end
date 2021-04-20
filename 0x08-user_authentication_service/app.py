2d4e5123e2374057172b1b5afc84a8fc2efe48c8
peluza <edisonisaza@gmail.com>
2021-04-19 19:53:11 -0500

    Get reset password token

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -69,12 +69,32 @@ def logout():
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
+    """profile
+
+    Returns:
+        str: {"email": "<user email>"}
+    """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": "<user email>"})
     else:
         abort(403)
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """get_reset_password_token
+
+    Returns:
+        str: {"email": "<user email>", "reset_token": "<reset token>"}
+    """
+    try:
+        email = request.form.get("email")
+        new_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": "<user email>", "reset_token": "<reset token>"})
+    except Exception:
+        abort(403)
+
 
 
 if __name__ == "__main__":
