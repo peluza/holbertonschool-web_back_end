c2ef267cc87c955bd552bb8f216943cde85100e9
peluza <edisonisaza@gmail.com>
2021-04-19 20:12:41 -0500

    chage menors

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -3,7 +3,6 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
 
 
@@ -14,7 +13,6 @@ class User(Base):
         Base (obj): base declarative base
     """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
