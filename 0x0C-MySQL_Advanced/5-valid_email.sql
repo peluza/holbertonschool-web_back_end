3119fefd9d7bdad577e2b6f159a85479185f7dc7
peluza <edisonisaza@gmail.com>
2021-04-29 17:29:21 -0500

    Email validation to sent

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- valid email
+-- creates a trigger that resets the attribute
+DELIMITER |
+CREATE TRIGGER bool BEFORE UPDATE ON users
+FOR EACH ROW 
+BEGIN
+    IF NEW.email <> OLD.email THEN 
+    SET NEW.valid_email = 0;
+    END IF;
+END;
+|
