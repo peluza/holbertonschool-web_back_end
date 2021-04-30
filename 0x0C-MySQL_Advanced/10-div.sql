a0883ad99f78544036c3c2a97ffb911a3a6628fa
peluza <edisonisaza@gmail.com>
2021-04-29 22:26:09 -0500

    Safe divide

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- div
+-- creates a function SafeDiv
+DELIMITER |
+CREATE FUNCTION SafeDiv (a INT, b INT ) RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        SET  result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END;
+|
