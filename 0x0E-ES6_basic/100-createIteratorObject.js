cb7274c23c4b0d4d1cdad0def3674ceac5ea6271
peluza <edisonisaza@gmail.com>
2021-05-06 22:52:42 -0500

    Iterating through report objects

---
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+export default function createIteratorObject(report) {
+  let iterable = [];
+
+  if (!report.allEmployees || typeof report.allEmployees !== "object") {
+    return iterable;
+  }
+
+  iterable = {
+    *[Symbol.iterator]() {
+      for (const value of Object.values(report.allEmployees)) {
+        for (const i of value) {
+          yield i;
+        }
+      }
+    },
+  };
+
+  return iterable;
+}
