3026a56836c083b2680e51958d4db2a1ed265e00
peluza <edisonisaza@gmail.com>
2021-05-11 23:31:37 -0500

    Catch me if you can!

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: "success",
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn("Got a response from the API"));
+}
+
+export default handleResponseFromAPI;
