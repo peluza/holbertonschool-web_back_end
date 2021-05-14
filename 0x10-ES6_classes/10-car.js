4d48807c8f88e18bb84b97e1f82122c697631f71
peluza <edisonisaza@gmail.com>
2021-05-13 20:20:06 -0500

    Vroom

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}
+
+export default Car;
