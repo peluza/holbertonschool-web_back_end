4eff4cc9d09c2e577d674d451e95435274bf2db7
Edison <edisonisaza@gmail.com>
2021-05-18 22:52:04 -0500

    More mapping

---
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function getListStudentIds (students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const ids = students.map((item) => item.id);
+
+  return ids;
+}
+
+export default getListStudentIds;
