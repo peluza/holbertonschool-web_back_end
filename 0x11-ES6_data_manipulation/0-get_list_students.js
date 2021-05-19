697306b0ea39136b44c24c4d2e58dc8b5ad60abb
Edison <edisonisaza@gmail.com>
2021-05-18 22:51:28 -0500

    Basic list of objects

---
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function getListStudents () {
+  const obj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+
+  return obj;
+}
+
+export default getListStudents;
