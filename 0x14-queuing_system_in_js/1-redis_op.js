9261a4727d96aa08988eaa68268d6494ff1208df
Edison <edisonisaza@gmail.com>
2021-06-01 22:19:30 -0500

    . Node Redis client and basic operations

---
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+import redis from 'redis';
+
+const cli = redis.createClient();
+
+cli.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+cli.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+// Node Redis client and basic operations
+function setNewSchool(schoolName, value) {
+    cli.set(schoolName, value, redis.print);
+  }
+  
+function displaySchoolValue(schoolName) {
+    cli.get(schoolName, (err, res) => {
+      console.log(res);
+    });
+  }
+  
+  displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  displaySchoolValue('HolbertonSanFrancisco');
