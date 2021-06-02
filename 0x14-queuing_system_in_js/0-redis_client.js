7fb5146fa8e8f66b81297592b88f9822fe61bc39
Edison <edisonisaza@gmail.com>
2021-06-01 22:16:05 -0500

    Node Redis Client

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+import redis from 'redis';
+
+const cli = redis.createClient();
+
+cli.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+cli.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
